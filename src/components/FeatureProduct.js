881aa7d9d081340e4bf14b36cf0c350d51057b54
Harsh Sharma <hs7898753@gmail.com>
2023-08-28 20:23:05 +0000

    Update 12 files

---
+++ src/components/FeatureProduct.js
@@ -0,0 +1,79 @@
+import "./FeatureProduct.css";
+import Vegetable from "./Vegetables.jpg";
+import Fruits from "./Fruits.jpg";
+import Grains from "./grains.jpg";
+import Delivery from "./delivery.png";
+import Cashback from "./cashback.png";
+import Support from "./support.png";
+function FeatureProduct() {
+  return (
+    <>
+      <div className="featureproduct">
+        <div className="product">
+          <div class="card down">
+            <img src={Vegetable} alt="vegetable" />
+            <span>Vegetables</span>
+            <p>GET UP TO 60% OFF</p>
+          </div>
+          <div class="card up">
+            <img src={Fruits} alt="Fruits" />
+            <span>Fruits</span>
+            <p>GET UP TO 60% OFF</p>
+          </div>
+          <div class="card down">
+            <img src={Grains} alt="Grains" />
+            <span>Grains</span>
+            <p>GET UP TO 60% OFF</p>
+          </div>
+        </div>
+        <div className="heading">
+          <span>Our Feature Products</span>
+          <p>
+            We deliver packets of All fresh products direct from farms, straight
+            to your door.
+          </p>
+        </div>
+      </div>
+      <div className="mainLast">
+        <div className="services">
+          <section>
+            <img src={Delivery} alt="Delivery" style={{ width: "100px" }} />
+            <h2>
+              Free Delivery <br /> <p>Free Delivery</p>
+            </h2>
+          </section>
+          <section>
+            <img src={Support} alt="Support" />
+            <h2>
+              Best Online Support <br /> <p>Hours: 8am - 8 pm</p>
+            </h2>
+          </section>
+          <section>
+            <img src={Cashback} alt="Cashback" />
+            <h2>
+              Money Back <br /> <p>100% Money Backy Guaranteed</p>
+            </h2>
+          </section>
+        </div>
+        <div className="address">
+          <span>CheckOut Delivery Address...</span>
+          <div className="search">
+            <form onsubmit="event.preventDefault();" role="search">
+              <label for="search">Search for stuff</label>
+              <input
+                id="search"
+                type="search"
+                placeholder="Search..."
+                autofocus
+                required
+              />
+              <button type="submit">Go</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default FeatureProduct;
