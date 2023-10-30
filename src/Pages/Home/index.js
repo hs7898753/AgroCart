cfbecad951f890bfc1770e2e22d9e5c3b57d566c
Harsh Sharma <hs7898753@gmail.com>
2023-10-30 18:15:24 +0000

    Update 19 files

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,18 @@
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import Main from "../../components/Main";
+import "./index.css";
+import FeatureProduct from "../../components/FeatureProduct";
+
+function Home() {
+  return (
+    <div className="home">
+      <Navbar />
+      <Main />
+      <FeatureProduct />
+      <Footer className="footer" />
+    </div>
+  );
+}
+
+export default Home;
